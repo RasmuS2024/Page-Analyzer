270a896e1f1e500c22863c231c0981f82d14d646
RasmuS2024 <rasburkhanov@yandex.ru>
2025-02-17 21:12:38 +0300

    modified index.php, added templates/index.phtml

--- public/index.php
+++ public/index.php
@@ -1,22 +1,46 @@
 <?php
+session_start();
+require __DIR__ . '/../vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+use DI\Container;
 
-require __DIR__ . '/../vendor/autoload.php';
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+$router = $app->getRouteCollector()->getRouteParser();
 
-$app = AppFactory::create();
+$app->get('/', function ($request, $response) {
+    $messages = $this->get('flash')->getMessages();
+    $params = [
+      'flash' => $messages
+    ];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+})->setName('index');
 
+/*
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello, tt!!!");
+    return $response;
+});
+*/
+
+
+/*
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
     return $response;
 });
-
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello, Admin!");
-    return $response;
-});
-
+*/
 $app->run();
