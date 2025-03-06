afc09e737c00f9b960f4aeb66fb02408c827cb0f
RasmuS2024 <rasburkhanov@yandex.ru>
2025-03-06 22:13:00 +0300

    new files:   src/Check.php and /CheckRepository.php, and modified files for save info in database

---
+++ src/Check.php
@@ -0,0 +1,97 @@
+<?php
+namespace WPA;
+
+class Check
+{
+    private ?int $id = null;
+    private ?int $urlId = null;
+    private ?int $statusCode = null;
+    private ?string $h1 = null;
+    private ?string $title = null;
+    private ?string $description = null;
+    private ?string $createdDT = null;
+
+    public static function fromArray(array $checkData): Check
+    {
+        [$urlId, $statusCode, $h1, $title, $description, $createdDT] = $checkData;
+        $check = new Check();
+        $check->setUrlId($urlId);
+        $check->setStatusCode($statusCode);
+        $check->setH1($h1);
+        $check->setTitle($title);
+        $check->setDescription($description);
+        $check->setCreatedDT($createdDT);
+        return $check;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrlId(): ?int
+    {
+        return $this->urlId;
+    }
+
+    public function getStatusCode(): ?int
+    {
+        return $this->statusCode;
+    }
+
+    public function getH1(): ?string
+    {
+        return $this->h1;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function getCreatedDT(): ?string
+    {
+        return $this->createdDT;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setUrlId(int $urlId): void
+    {
+        $this->urlId = $urlId;
+    }
+
+    public function setStatusCode(string $statusCode): void
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    public function setH1(string $h1): void
+    {
+        $this->h1 = $h1;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function setCreatedDT(string $createdDT): void
+    {
+        $this->createdDT = $createdDT;
+    }
+
+}
---
+++ src/CheckRepository.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace WPA;
+
+class CheckRepository
+{
+    private \PDO $conn;
+
+    public function __construct(\PDO $conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function getEntities(): array
+    {
+        $checks = [];
+        $sql = "SELECT * FROM url_checks ORDER BY id DESC";
+        $stmt = $this->conn->query($sql);
+        while ($row = $stmt->fetch()) {
+            $check = Check::fromArray([$row['url_id'], $row['status_code'], $row['h1'], $row['title'], $row['description'], $row['created_at']]);
+            $check->setId($row['id']);
+            $checks[] = $check;
+        }
+        return $checks;
+    }
+
+    public function find(int $id): ?Check
+    {
+        $sql = "SELECT * FROM url_checks WHERE id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id]);
+        if ($row = $stmt->fetch())  {
+            $check = Check::fromArray([$row['url_id'], $row['status_code'], $row['h1'], $row['title'], $row['description'], $row['created_at']]);
+            $check->setId($row['id']);
+            return $check;
+        }
+        return null;
+    }
+
+    public function findAllUrlId(int $id): array
+    {
+        $checks = [];
+        $sql = "SELECT * FROM url_checks WHERE url_id = ? ORDER BY id DESC";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id]);
+        while ($row = $stmt->fetch()) {
+            $check = Check::fromArray([$row['url_id'], $row['status_code'], $row['h1'], $row['title'], $row['description'], $row['created_at']]);
+            $check->setId($row['id']);
+            $checks[] = $check;
+        }
+        return $checks;
+    }
+/*
+    public function save(Check $check): int 
+    {
+        if ($check->exists()) {
+            $id = $this->update($check);
+        } else {
+            $id = $this->create($check);
+        }
+        return $id;
+    }
+*/
+    private function update(Check $check): int
+    {
+        $sql = "UPDATE url_checks 
+        SET url_id = :urlId, 
+        status_code = :statusCode, 
+        h1 = :h1, 
+        title = :title, 
+        description = :description, 
+        created_at = :createdDT 
+        WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $id = $check->getId();
+        $urlId = $check->getUrlId();
+        $statusCode = $check->getStatusCode();
+        $h1 = $check->getH1();
+        $title = $check->getTitle();
+        $description = $check->getDescription();
+        $createdDT = $check->getCreatedDT();
+        $stmt->bindParam(':url_id', $urlId);
+        $stmt->bindParam(':statusCode', $statusCode);
+        $stmt->bindParam(':h1', $h1);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':createdDT', $createdDT);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $urlId;
+    }
+
+    public function create(Check $check): void
+    {
+        $sql = "INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at) VALUES (:url_id, :statusCode, :h1, :title, :description, :createdDT)";
+        $stmt = $this->conn->prepare($sql);
+        $urlId = $check->getUrlId();
+        $statusCode = $check->getStatusCode();
+        $h1 = $check->getH1();
+        $title = $check->getTitle();
+        $description = $check->getDescription();
+        $createdDT = $check->getCreatedDT();
+        $stmt->bindParam(':url_id', $urlId);
+        $stmt->bindParam(':statusCode', $statusCode);
+        $stmt->bindParam(':h1', $h1);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':createdDT', $createdDT);
+        $stmt->execute();
+        $id = (int) $this->conn->lastInsertId();
+        $check->setId($id);
+        //return $urlId;
+    }
+}
